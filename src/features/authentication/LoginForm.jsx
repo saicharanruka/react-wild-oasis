764e8b2c709130df3208cd5c9242ccb2645e8cc6
slayer0x10 <saicharanruka@gmail.com>
2025-07-13 19:42:30 +0530

    added login page

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,40 +2,52 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import FormRowVertical from "../../ui/FormRowVertical.jsx";
+import { useLogin } from "./useLogin.js";
+import SpinnerMini from "../../ui/SpinnerMini.jsx";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (!email || !password) return;
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
-        <Input
-          type="email"
-          id="email"
-          // This makes this form better for password managers
-          autoComplete="username"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormRowVertical>
-      <FormRowVertical label="Password">
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormRowVertical>
-      <FormRowVertical>
-        <Button size="large">Login</Button>
-      </FormRowVertical>
-    </Form>
-  );
+		login({ email, password });
+	}
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<FormRowVertical label="Email address">
+				<Input
+					type="email"
+					id="email"
+					// This makes this form better for password managers
+					autoComplete="username"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					disabled={isLoading}
+				/>
+			</FormRowVertical>
+			<FormRowVertical label="Password">
+				<Input
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					disabled={isLoading}
+				/>
+			</FormRowVertical>
+			<FormRowVertical>
+				<Button size="large" disabled={isLoading}>
+					{isLoading ? <SpinnerMini /> : "Log in"}
+				</Button>
+			</FormRowVertical>
+		</Form>
+	);
 }
 
 export default LoginForm;
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
-const Heading = styled.h1`
+const Heading = styled.h4`
 	line-height: 1.4;
+	font-size: 3rem;
+	font-weight: 600;
+	text-align: center;
 `;
 
 export default Heading;
